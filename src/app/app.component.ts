bed569d5b29035aa006ca9290c2bbdc5dc84b9ed
shinjh0849 <21200421@handong.edu>
2017-07-19 16:26:05 +0900

    루트페이지 홈페이지로 다시 바꾼거 ㅇㅇ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-
 import { HomePage } from '../pages/home/home';
-import { T12Page } from '../pages/t12/t12';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = T12Page;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
