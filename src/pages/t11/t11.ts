e5b05b4b4a863397cf777a2209979551c5b959fc
wisdomjhkwon <wisdomjhkwon@gmail.com>
2017-07-19 16:30:37 +0900

    11페이지 안녕

--- src/pages/t11/t11.ts
+++ src/pages/t11/t11.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Input, ElementRef, Renderer, Output, EventEmitter } from '@angular/core';
+import { DomController } from 'ionic-angular';
 
-/**
- * Generated class for the T11Page page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- * 
- * @IonicPage()
- * 
- */
+import { T10Page } from '../t10/t10';
+import { T12Page } from '../t12/t12';
 
 @Component({
   selector: 'page-t11',
@@ -17,11 +12,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class T11Page {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad T11Page');
   }
 
+  onTq(){
+    this.navCtrl.pop();
+  }
+
+  onDoubleTap(e) {
+    console.log("singleTap from DoubleTapDirective")
+    //this.navCtrl.push(HomePage, {}, { animate: false }); // 다시 들으시려면 탭 두번을 (지금은 처음페이지로 이동)
+  }
+
+  onShortPress(e) {
+    console.log("shortPress from ShortPressDirective")
+    this.navCtrl.push( T12Page , {}, { animate: false });
+  }
 }
