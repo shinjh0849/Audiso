bb61d04e06328cc07556469bb17cf33dd67d200b
TOSHIBA\user <21500420@handong.edu>
2017-07-19 17:01:15 +0900

    T5, T8, T9 완료(순서도 완료 )

--- src/pages/t9/t9.ts
+++ src/pages/t9/t9.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { T8Page } from "../t8/t8";
+import { T10Page } from "../t10/t10";
 import { HomePage } from "../home/home";
 /**
  * Generated class for the T9Page page.
@@ -26,11 +27,11 @@ export class T9Page {
 
   onDoubleTap(e) {
     console.log("singleTap from DoubleTapDirective")
-    this.navCtrl.push(HomePage, {}, { animate: false }); // 다시 들으시려면 탭 두번을 (지금은 처음페이지로 이동)
+    //this.navCtrl.push(HomePage, {}, { animate: false }); // 다시 들으시려면 탭 두번을 (지금은 처음페이지로 이동)
   }
 
   onShortPress(e) {
     console.log("shortPress from ShortPressDirective")
-    //this.navCtrl.push(T10Page, {}, { animate: false });
+    this.navCtrl.push(T10Page, {}, { animate: false });
   }
 }
