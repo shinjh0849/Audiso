2efb8ec170bef351b1a9477f38d7c9fe371325ea
shinjh0849 <21200421@handong.edu>
2017-07-21 13:53:49 +0900

    신지 큐알코드 작업 & 네이티브 오디오 작업 내용 업로드 후 충돌부분 보정

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
-import { T5Page } from './../t5/t5';
-import { T2Page } from './../t2/t2';
-
-
-
-=======
->>>>>>> JihoShin
 import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
 import { App } from 'ionic-angular';
 
+import { T2Page } from './../t2/t2';
 import { T4Page } from './../t4/t4';
+import { T5Page } from './../t5/t5';
+
 import { QRcodePage } from '../q-rcode/q-rcode';
 import { NativeAudio } from '@ionic-native/native-audio';
 
@@ -31,24 +26,21 @@ export class HomePage {
 
 
   onClick(){
-<<<<<<< HEAD
-
-   
     this.navCtrl.push(T2Page);
-=======
-    //this.navCtrl.push(T4Page);
   }
->>>>>>> JihoShin
 
-  QRbutton(){
-    this.navCtrl.push(QRcodePage);
-  }
+
 
   ionViewDidLoad(){
     console.log('HOME PAGE');
   }
 
-  //이걸 ionViewDidLoad 메써드에 넣게 되면 자동적으로 노래가 나온다
+  //이버튼을 누르면 큐알코드 작동하는 페이지로 넘어감
+  QRbutton(){
+    this.navCtrl.push(QRcodePage);
+  }
+
+  //이버튼을 누르면 노래가 나옴 -> 나중에 메써드 안에 있는 코드를 ionViewDidLoad에 넣으면 페이지 로딩때 노래가 나옴
   PLAY(){
   this.nativeAudio.preloadComplex('test1', 'assets/You-lose-sound-effect.mp3', 1, 1, 0).then(() => {
     this.nativeAudio.play('test1', () => console.log('test1 is done playing'));
