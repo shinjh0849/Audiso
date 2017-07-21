36040b4068989177699d89d9707df859f9ea8e48
shinjh0849 <21200421@handong.edu>
2017-07-21 17:00:35 +0900

    신지 빠운스 빠운스 두근되 홈페이지 갖고 좀 놀았음

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, Platform } from 'ionic-angular';
-import { App } from 'ionic-angular';
+import { App, NavController, NavParams, Platform } from 'ionic-angular';
 
 import { T2Page } from './../t2/t2';
 import { T4Page } from './../t4/t4';
@@ -26,7 +25,7 @@ export class HomePage {
 
 
   onClick(){
-    this.navCtrl.push(T2Page);
+    //this.navCtrl.push(T2Page);
   }
 
 
@@ -42,9 +41,15 @@ export class HomePage {
 
   //이버튼을 누르면 노래가 나옴 -> 나중에 메써드 안에 있는 코드를 ionViewDidLoad에 넣으면 페이지 로딩때 노래가 나옴
   PLAY(){
-  this.nativeAudio.preloadComplex('test1', 'assets/You-lose-sound-effect.mp3', 1, 1, 0).then(() => {
-    this.nativeAudio.play('test1', () => console.log('test1 is done playing'));
-  })
-  this.nativeAudio.unload('test1');
+    this.nativeAudio.preloadComplex('test1', 'assets/You-lose-sound-effect.mp3', 1, 1, 0).then(() => {
+      this.nativeAudio.play('test1', () => console.log('test1 is done playing'));
+    })
+    this.nativeAudio.unload('test1');
   }
+
+  bounce(){
+    
+  }
+  
+      
 }
